d9ee5bbdf1c760e6e46433a4abab846de2f54698
Kristen Fang <kf4538@rit.edu>
2024-06-12 16:56:21 -0400

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div className="flex flex-col items-center ">
             <h1>Yang's Project</h1>
-            <p>Please participate in this study </p>
+            <p>Please participate in this study</p>
             <h4>Click to begin!</h4>
 
             <LinkButton
