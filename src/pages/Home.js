0ab1ef47302c396fc91b2e425e61aa2b6ce4e293
Kristen Fang <kf4538@rit.edu>
2024-06-12 16:40:39 -0400

    change title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import LinkButton from "../components/LinkButton";
 function Home() {
     return (
         <div className="flex flex-col items-center ">
-            <h1>Yang's Goofy ass Project</h1>
+            <h1>Yang's Project</h1>
             <p>Please participate in this study</p>
             <h4>Click to begin!</h4>
 
